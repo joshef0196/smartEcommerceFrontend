bccfaae132bb6e3abe2861a970c91b188148ff0c
Joshef Hossain <53108556+joshef0196@users.noreply.github.com>
2024-10-08 00:55:13 +0600

    Header componet added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header'
+
+export default function Home() {
+  return (
+    <div>
+        <Header></Header>
+        <h1>Home page</h1>
+    </div>
+  )
+}
