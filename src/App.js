6f2b5ac17c5ded9786c22e665ea0bf1d2f2bb37f
Joshef Hossain <53108556+joshef0196@users.noreply.github.com>
2024-10-13 00:42:09 +0600

    Shop done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css'; 
-import Header from './pages/Header';
+import './App.css';  
 import Home from './pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Shop from './pages/Shop';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home />}> </Route>
+            <Route path='/shop' element={<Shop />}> </Route>
         </Routes>
     </BrowserRouter> 
+    // <Home />
   );
 }
 
